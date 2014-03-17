410dd10d5bbe8cb8f56c5ad4b5dad60a6bfa694a
Valera Rozuvan <valera@edx.org>
2014-03-17 19:54:57 +0200

    Refactoring random_string.js file to include proper JSLint info.

--- js/random_string.js
+++ js/random_string.js
@@ -1,8 +1,28 @@
-(function (window, undefined) {
+/*global
+    define: false, console: true
+*/
+
+/*jslint
+    browser: false, white: false, indent: 4, maxlen: 120, nomen: true, plusplus: false
+*/
+
+/*properties
+    length, random, round, hasOwnProperty, log
+*/
+
+(function () {
     'use strict';
 
-    // Our module.
-    var randomString = function () {
+    // Variables available within this closure.
+    var _mask = '',
+        _length = 16,
+        _history = {},
+
+        modDeps,
+        defineCallback,
+
+        // Our module.
+        randomString = function () {
             var result, i;
 
             do {
@@ -10,7 +30,8 @@
 
                 // The magic that creates the random string of length
                 // `_length`, containing symbols from `_mask`.
-                for (i = _length; i > 0; --i) {
+                for (i = _length; i > 0; i -= 1) {
+                    console.log('[randomString]: i = ', i);
                     result += _mask[Math.round(Math.random() * (_mask.length - 1))];
                 }
             } while (_history.hasOwnProperty(result));
@@ -20,22 +41,18 @@
             _history[result] = 1;
 
             return result;
-        },
-
-        // Variables available within this closure.
-        _mask = '',
-        _length = 16,
-        _history = {};
+        };
 
     // What characters do we want our random string to consist of?
     _mask += 'abcdefghijklmnopqrstuvwxyz';
     _mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-
-    // Export the module.
-
-    // Make the randomString module available via Require JS `define()` method.
-    define(function () {
+    defineCallback = function () {
         return randomString;
-    });
-}).call(this, this);
+    };
+
+    // Module dependencies.
+    modDeps = [];
+
+    define(modDeps, defineCallback);
+}());
